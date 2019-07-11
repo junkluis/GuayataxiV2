1b4645a4a42a8c55f5d20cba9cee9d3b178995dc
AnibalG <aagamboa@espol.edu.ec>
2019-07-11 11:21:53 -0500

    pruebas cliente y Ubicacion

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,4 +1,38 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Billetera;
+import guayataxi.Cliente;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
 public class ClienteTest {
+    Billetera billeteraPrueba = null;
+
+    @Before
+    public void iniciarPruebas(){
+        billeteraPrueba = new Billetera(2);
+        billeteraPrueba.agregarFondos(10);
+    }
+
+    @Test
+    public void crearNuevoCliente(){
+        boolean creacionExito = false;
+        String nombre = "jose fuentes";
+        String cedula = "09224509";
+        try{
+            Cliente cliente = new Cliente(nombre, cedula, billeteraPrueba);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertTrue(creacionExito);
+
+    }
+
+
 }
--- app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
+++ app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
@@ -1,4 +1,79 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Ubicacion;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class UbicacionTest {
+    Ubicacion ubicacionPrueba;
+
+    @Test
+    public void crearNuevaUbicacion(){
+        boolean creacionExito;
+        float longitud = (float)10.5;
+        float latitud = (float)10.5;
+        float distancia = (float) 38.5;
+        try{
+            Ubicacion nuevaUbicacion = new Ubicacion(longitud, latitud, distancia);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertTrue(creacionExito);
+
+    }
+
+    @Test
+    public void calcularPrecioMenor10(){
+        float precioCalculado;
+        ubicacionPrueba = new Ubicacion((float)12.5, (float) 9.5, (float) 5);
+        precioCalculado = ubicacionPrueba.calcularPrecio();
+        assertEquals((float)2, precioCalculado,0);
+
+    }
+    @Test
+    public void calcularPrecioMenor30(){
+        float precioCalculado;
+        ubicacionPrueba = new Ubicacion((float)12.5, (float) 9.5, (float) 12);
+        precioCalculado = ubicacionPrueba.calcularPrecio();
+        assertEquals((float)5, precioCalculado,0);
+
+    }
+
+    @Test
+    public void calcularPrecioLongitudmayor50(){
+        float precioCalculado;
+        ubicacionPrueba = new Ubicacion((float)70, (float) 9.5, (float) 35);
+        precioCalculado = ubicacionPrueba.calcularPrecio();
+        assertEquals((float)10, precioCalculado,0);
+
+    }
+    @Test
+    public void calcularPrecioLaitudmayor50(){
+        float precioCalculado;
+        ubicacionPrueba = new Ubicacion((float)12, (float) 75, (float) 35);
+        precioCalculado = ubicacionPrueba.calcularPrecio();
+        assertEquals((float)15, precioCalculado,0);
+
+    }
+
+    @Test
+    public void SetDescriptionTest(){
+        boolean setExito;
+        ubicacionPrueba = new Ubicacion((float)12, (float) 75, (float) 35);
+        try {
+            ubicacionPrueba.setDescripcion("descrpcion");
+            setExito = true;
+        }
+        catch (AssertionError error){
+            setExito = false;
+        }
+    assertTrue(setExito);
+
+
+    }
 }
