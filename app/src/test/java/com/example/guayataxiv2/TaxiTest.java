3ff8e5066c77071fc948590199db72bb0787ddeb
wmartine32 <wellinmart32@gmail.com>
2019-07-11 13:41:09 -0500

    Update TaxiTest.java

--- app/src/test/java/com/example/guayataxiv2/TaxiTest.java
+++ app/src/test/java/com/example/guayataxiv2/TaxiTest.java
@@ -1,4 +1,17 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Taxi;
+
 public class TaxiTest {
+  
+  Taxi taxiValido = null;
+  Taxi taxiNoValido = null;
+  
+  public iniciarPruebas() {
+    taxiValido = new Taxi("GCZ567", "Hiundai", 2);
+    taxiNoValido = new Taxi("GCV653", "Renaul", 3);
+  }
 }
