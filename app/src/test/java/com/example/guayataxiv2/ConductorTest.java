ec778e45f3e9b7b4d2901a03059ef3a987a2a948
wmartine32 <wellinmart32@gmail.com>
2019-07-11 13:23:46 -0500

    Update ConductorTest.java

--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -7,9 +7,11 @@ import guayataxi.Taxi;
 
 public class ConductorTest {
     Conductor conductorPrueba = null;
+    Conductor conductorSinTaxi = null;
     
     public iniciarPruebas() {
         conductorPrueba = new Conductor("Wellington Matinez", "0937276890");
+        ConductorSinTaxi = new Conductor("Clara Alcazar", "0955778979");
     }
     
     public void asignarTaxi() {
@@ -22,5 +24,17 @@ public class ConductorTest {
             asignacionExitosa = false;
         }
         assertEquals(true, asignacionExitosa);        
+    }
+    
+    public void sinTaxi() {
+        Taxi taxi = null;
+        boolean asignacionExitosa = false;        
+        try {
+            conductorPrueba.asignarTaxi(taxi);
+            asignacionExitosa = true;
+        } catch (AssertionError error){
+            asignacionExitosa = false;
+        }
+        assertEquals(true, asignacionExitosa);         
     }
 }
