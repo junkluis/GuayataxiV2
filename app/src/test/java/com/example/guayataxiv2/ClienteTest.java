ac3b7f42d7468df4809d6375b9195b99887692a3
Byron Povea <bpovea@espol.edu.ec>
2019-07-11 10:58:39 -0500

    [UPD] first test - create Cliente

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,4 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Cliente;
+import guayataxi.Billetera;
+
+import static org.junit.Assert.*;
+
 package com.example.guayataxiv2;
 
 public class ClienteTest {
+
+    Billetera billeteraPrueba = null;
+
+
+    @Before
+    public void iniciarPruebas(){
+        billeteraPrueba = new Billetera(2);
+        billeteraPrueba.agregarFondos(10);
+    }
+
+    @Test
+    public void crearCliente(){
+        boolean creacionExito = false;
+        try{
+            Cliente clienteByron = new Cliente('Byron Povea','1726873431',billeteraPrueba);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
 }
