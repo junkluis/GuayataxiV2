6b07d877acec705634ece469ad80ce0c3d2f5d31
wmartine32 <wellinmart32@gmail.com>
2019-07-11 13:48:50 -0500

    Update TaxiTest.java

--- app/src/test/java/com/example/guayataxiv2/TaxiTest.java
+++ app/src/test/java/com/example/guayataxiv2/TaxiTest.java
@@ -14,7 +14,14 @@ public class TaxiTest {
     taxiValido = new Taxi("GCZ567", "Hiundai", 2);
     taxiNoValido = new Taxi("GCV653", "Renaul", 3);
     
-    taxiValido.setColor("Rojo");
-    taxiNoValido.setColor("Negro");
+
+  }
+  
+  public void confirmarColores() {
+    String mensaje1 =taxiValido.setColor("Rojo");
+    String mensaje2 = taxiNoValido.setColor("Negro");
+    
+    stringEquals("Al taxi se le asigno el color: Rojo", mensaje1);
+    stringEquals("Debe seleccionar un color dentor de la lista posibles", mensaje2);
   }
 }
