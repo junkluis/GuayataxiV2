99d2b7e396bf3c4c32e5ad0f0f0d76fb11f6b080
wmartine32 <wellinmart32@gmail.com>
2019-07-11 12:57:21 -0500

    Prueba cliente actualizada

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -11,21 +11,24 @@ public class ClienteTest {
     Cliente clientePrueba = null;
 
     public void iniciarPruebas() {
-        Ubicacion ubicacion = new Ubicacion(20051, 55450, 33796);
         Billetera billetera = new Billetera(1);
-        clientePrueba = new Cliente("Mildred", "0934869211", ubicacion, billetera);
+        clientePrueba = new Cliente("Mildred", "0934869211", billetera);
     }
     
     @Test
      public void crearUnNuevoCliente() {
         boolean creacionExitosa = false;
-        Ubicacion ubicacion = new Ubicacion(30099843, 223284, 3332223);
         Billetera billetera = new Billetera(2);
         try {
-            Cliente clienteJuan = new Cliente("Juan", "0934285443", ubicacion, billetera);
+            Cliente clienteJuan = new Cliente("Juan", "0934285443", billetera);
             creacionExitosa = true;
         }catch(AssertionError error) {
             creacionExitosa = false;
         }
-         assertEquals(true, creacionExitosa);
+        assertEquals(true, creacionExitosa);
+     }
+    
+    public void ubicarCasa() {
+        clientePrueba.registrarCasa(33227, 3328217, 337226);
+    }
 }
