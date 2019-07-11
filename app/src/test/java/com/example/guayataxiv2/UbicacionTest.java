7cba051407439aa5b0a0d7d55ea1d01c3040cd43
wmartine32 <wellinmart32@gmail.com>
2019-07-11 13:59:59 -0500

    Update UbicacionTest.java

--- app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
+++ app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
@@ -1,4 +1,15 @@
 package com.example.guayataxiv2;
 
 public class UbicacionTest {
+  Ubicacion u1 = null, u2 = null, u3 = null, u4 = null;
+  
+  public void iniciarPruebas() {
+    u1 = new Ubicacion(4, 4, 4);
+    u2 = new Ubicacion(340, 435, 34);
+    u3 = new Ubicacion(132, 12, 48);
+    u3 = new Ubicacion(12, 443, 67);
+  }
+  
+  public void verPrecios() {
+  }
 }
