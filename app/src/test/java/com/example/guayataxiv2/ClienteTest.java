9d615e80237c17516bef7370f1b37bd9596755db
Byron Povea <bpovea@espol.edu.ec>
2019-07-11 11:08:15 -0500

    [UPD] registrar casa

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 import guayataxi.Cliente;
 import guayataxi.Billetera;
+import guayataxi.Ubicacion;
 
 import static org.junit.Assert.*;
 
@@ -11,23 +12,32 @@ package com.example.guayataxiv2;
 public class ClienteTest {
 
     Billetera billeteraPrueba = null;
+    Cliente clientePrueba = null;
 
 
     @Before
     public void iniciarPruebas(){
         billeteraPrueba = new Billetera(2);
         billeteraPrueba.agregarFondos(10);
+        clientePrueba = new Cliente('Yesenia Murillo','0913852890',billeteraPrueba);
     }
 
     @Test
     public void crearCliente(){
         boolean creacionExito = false;
         try{
-            Cliente clienteByron = new Cliente('Byron Povea','1726873431',billeteraPrueba);
+            Cliente clienteByron = new Cliente('Byron Povea','1726273531',billeteraPrueba);
             creacionExito = true;
         } catch (AssertionError error){
             creacionExito = false;
         }
         assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void testRegistrarCasa(){
+        Ubicacion miCasa = null;
+        miCasa = clientePrueba.registrarCasa(-2.1,16.5,500)
+        assertNotEquals(null,miCasa)
     }
 }
