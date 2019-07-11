cc38916f18764de73b0e6355ee6b8a431eeced9d
wmartine32 <wellinmart32@gmail.com>
2019-07-11 12:51:32 -0500

    Actualizacion de ClienteTest

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,27 +1,28 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Billetera;
+import guayataxi.Ubicacion;
+import guayataxi.Cliente;
+
 public class ClienteTest {
     Cliente clientePrueba = null;
-    Ubicacion ubicacion = null;
-    Billetera billetera = null;
 
     public void iniciarPruebas() {
-        ubicacion = registrarCasa(20051, 55450, 33796);
-        billetera = new Billetera(1);
+        Ubicacion ubicacion = new Ubicacion(20051, 55450, 33796);
+        Billetera billetera = new Billetera(1);
         clientePrueba = new Cliente("Mildred", "0934869211", ubicacion, billetera);
-    }
-
-    public Ubicacion registrarCasa(float px, float py, float pz) {
-        Ubicacion ubicacion = new Ubicacion(px, py, pz);
-        return ubicacion;
     }
     
     @Test
      public void crearUnNuevoCliente() {
         boolean creacionExitosa = false;
-        Ubicacion ubicacion = registrarCasa(30099843, 223284, 3332223);
+        Ubicacion ubicacion = new Ubicacion(30099843, 223284, 3332223);
+        Billetera billetera = new Billetera(2);
         try {
-            Cliente clienteJuan = new Cliente("Juan", "0934285443", ubicacion);
+            Cliente clienteJuan = new Cliente("Juan", "0934285443", ubicacion, billetera);
             creacionExitosa = true;
         }catch(AssertionError error) {
             creacionExitosa = false;
