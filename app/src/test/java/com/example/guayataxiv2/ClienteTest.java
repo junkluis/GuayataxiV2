f1180671e26a1de14a2ebd15fabbbc481e0b11aa
Cliente CONTROL-PC <CltControl@GGIECSOFWRK131.espol.edu.ec>
2019-07-11 11:25:39 -0500

    update

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -19,7 +19,8 @@ public class ClienteTest {
 
 	@Before
     public void iniciarPruebas(){
-        clientePrueba = new Cliente("Sebastian Ramirez", "0954225447", billetera);
+        billeteraPrueba = new Billetera(1);
+        clientePrueba = new Cliente("Sebastian Ramirez", "0954225447", billeteraPrueba);
     }
 
 
@@ -52,8 +53,6 @@ public class ClienteTest {
     public void registrarCasa(){
         boolean creacionExito = false;
         try{
-        	billeteraPrueba = new Billetera(1);
-            clientePrueba = new Cliente("Sebastian Ramirez", "0954225447", billeteraPrueba);
         	ubicacionPrueba = clientePrueba.registrarCasa(2.3456456, 2.3456456, 450.26);
             creacionExito = true;
         } catch (AssertionError error){
--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -1,4 +1,14 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Conductor;
+
+import static org.junit.Assert.*;
+
+
 public class ConductorTest {
+
+	Conductor conductorPrueba = null;
 }
