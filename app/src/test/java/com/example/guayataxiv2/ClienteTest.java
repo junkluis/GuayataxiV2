9aa5fe286e3ab237129a0d7805e0b82c24801a67
mdleiton <mau_leiton96@hotmail.com>
2019-07-11 10:59:10 -0500

    [upd] test en conductor y cliente

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,4 +1,18 @@
 package com.example.guayataxiv2;
 
 public class ClienteTest {
+
+    public Cliente cliente = null;
+
+    @Before
+    public void iniciarPruebas(){
+        Billetera billeteraPrueba = new Billetera(2);
+        this.cliente = new Cliente("Mauricio Leiton Lazaro", "0989389265", billeteraPrueba);
+    }
+
+    @Test
+    public void registrarCasa(){
+        Ubicacion miCasa = this.cliente.registrarCasa(12.2,12.2,12.2);
+        assertTrue(miCasa instanceof Ubicacion);
+    }
 }
--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -1,4 +1,43 @@
 package com.example.guayataxiv2;
 
 public class ConductorTest {
+
+    @Test
+    public void verificarCedulaInvalida5digitos(){
+        Coductor conductor_1 = new Coductor("Mauricio Leiton Lazaro", "09893");
+        boolean valida = conductor_1.verificarCedula();
+        assertTrue(!valida);
+    }
+
+    @Test
+    public void verificarCedulaInvalida10digitos0Inicial(){
+        Coductor conductor_1 = new Coductor("Mauricio Leiton Lazaro", "0989389265");
+        boolean valida = conductor_1.verificarCedula();
+        assertTrue(valida);
+    }
+
+    @Test
+    public void verificarCedulaInvalida10digitos1Inicial(){
+        Coductor conductor_1 = new Coductor("Mauricio Leiton Lazaro", "1989389265");
+        boolean valida = conductor_1.verificarCedula();
+        assertTrue(!valida);
+    }
+
+    @Test
+    public void asignarTaxiNulo(){
+        Coductor conductor_1 = new Coductor("Mauricio Leiton Lazaro", "0989389265");
+        boolean valida = conductor_1.verificarCedula();
+        Taxi taxi = null;
+        boolean validaTaxi = conductor_1.asignarUnTaxi(taxi);
+        assertTrue(!validaTaxi);
+    }
+
+    @Test
+    public void asignarTaxiOk(){
+        Coductor conductor_1 = new Coductor("Mauricio Leiton Lazaro", "0989389265");
+        boolean valida = conductor_1.verificarCedula();
+        Taxi taxi = new Taxi("GYA1223", "Mazda", 1);
+        boolean validaTaxi = conductor_1.asignarUnTaxi(taxi);
+        assertTrue(validaTaxi);
+    }
 }
