5e37cf0616efe5abf50641d79eb83c0fa32111df
Cliente CONTROL-PC <CltControl@GGIECSOFWRK146.espol.edu.ec>
2019-07-11 11:27:32 -0500

    Pruebas con cliente

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,4 +1,51 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Billetera;
+import guayataxi.Cliente;
+
+import static org.junit.Assert.*;
+
+
 public class ClienteTest {
+
+    Billetera billeteraPrueba = null;
+    Cliente clientePrueba = null;
+    Cliente clienteSinFondos = null;
+
+    @Before
+    public void iniciarPruebas(){
+        billeteraPrueba = new Billetera(2);
+        billeteraPrueba.agregarFondos(10);
+
+        clientePrueba = new Cliente("Jose Masson", "0931753354", billeteraPrueba);
+    }
+
+    @Test
+    public void crearNuevoCliente(){
+        boolean creacionExito = false;
+        try{
+            Billetera billeteraJose = new Billetera(1);
+            Cliente clienteJose = new Cliente("Jose Masson", "0931753354", billeteraJose);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void registrarCasa() {
+        boolean registroExito = false;
+        try {
+            Ubicacion casa = clientePrueba.registrarCasa(-2.3, -79.3, 20);
+            registroExito = true;
+        } catch (AssertionError error){
+            registroExito = false;
+        }
+        assertEquals(true, registroExito);
+    }
+
 }
