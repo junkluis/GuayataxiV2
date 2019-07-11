e74f22b5f2d5b4764b79c1ade145004eec8b5b11
Byron Povea <bpovea@espol.edu.ec>
2019-07-11 17:00:13 -0500

    [FIX] double to float

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -19,14 +19,14 @@ public class ClienteTest {
     public void iniciarPruebas(){
         billeteraPrueba = new Billetera(2);
         billeteraPrueba.agregarFondos(10);
-        clientePrueba = new Cliente('Yesenia Murillo','0913852890',billeteraPrueba);
+        clientePrueba = new Cliente("Yesenia Murillo","0913852890",billeteraPrueba);
     }
 
     @Test
     public void crearCliente(){
         boolean creacionExito = false;
         try{
-            Cliente clienteByron = new Cliente('Byron Povea','1726273531',billeteraPrueba);
+            Cliente clienteByron = new Cliente("Byron Povea","1726273531",billeteraPrueba);
             creacionExito = true;
         } catch (AssertionError error){
             creacionExito = false;
@@ -37,7 +37,7 @@ public class ClienteTest {
     @Test
     public void testRegistrarCasa(){
         Ubicacion miCasa = null;
-        miCasa = clientePrueba.registrarCasa(-2.1,16.5,500);
+        miCasa = clientePrueba.registrarCasa(-2.1f, 16.5f,500);
         assertNotEquals(null,miCasa);
     }
 }
--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -1,4 +1,35 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Conductor;
+import guayataxi.Taxi;
+
+import static org.junit.Assert.*;
+
 public class ConductorTest {
+
+    Conductor conductorPrueba = null;
+    Taxi taxi = null;
+
+    @Before
+    public void iniciarPruebas(){
+        conductorPrueba = new Conductor("conductor prueba","0987654321");
+    }
+
+    @Test
+    public void crearConductor(){
+        boolean creacionExito = false;
+        try{
+            Conductor nuevoConductor = new Conductor("Jose Luis","1234567890");
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+
+
 }
