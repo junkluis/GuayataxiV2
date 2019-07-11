aa4e6e2f1ea05383dee2ec2524d8db55e0d0f3f8
Cliente CONTROL-PC <CltControl@GGIECSOFWRK138.espol.edu.ec>
2019-07-11 11:14:33 -0500

    pruebas cliente

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,4 +1,42 @@
 package com.example.guayataxiv2;
 
 public class ClienteTest {
+
+
+    Cliente clienteubicacionreal = null;
+
+
+    @Before
+    public void iniciarPruebas(){
+
+
+
+        clientePrueba = new Cliente(2);
+        billeteraPrueba.agregarubicacion(10);
+
+
+    }
+
+    @Test
+    public void crearcliente(){
+        boolean creacionExito = false;
+        try{
+            Cliente clientedaniel = new Cliente(1);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void agregarubicacion(){
+        String mensajeEsperado = "Se agregaron la ubicacion con exito";
+        String valor = clienteubicacionreal.agregarubicacion(50);
+        assertEquals(mensajeEsperado, valor);
+    }
+
+
+
+
 }
