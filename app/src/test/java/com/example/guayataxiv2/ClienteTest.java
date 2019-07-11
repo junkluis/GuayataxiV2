5fcf8f76cbe30a4461a210812b5ac14f8080f4b9
Carlos Eduardo Veintemilla Arias <ceva94@hotmail.com>
2019-07-11 11:20:46 -0500

    pruebas

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,4 +1,49 @@
 package com.example.guayataxiv2;
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Billetera;
+import guayataxi.Ubicacion;
+import guayataxi.Cliente;
+
+import static org.junit.Assert.*;
 
 public class ClienteTest {
+    CLiente cliente = null;
+    Ubicacion ubicacion = null;
+    Billetera billetera = null;
+
+    @Before
+    public void iniciarPruebas(){
+        billetera = new Billetera(2);
+        billetera.agregarFondos(10);
+
+        cliente = new Cliente("Carlos Veintemilla","0705786564",billetera);
+        billeteraSinFondos = new Billetera(3);
+    }
+
+    @Test
+    public void agregarUnaUbicacion(){
+        boolean creacionExito = false;
+        try{
+            cliente.registrarCasa(1.1, 1.1, 10.1);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void crearNuevoCliente(){
+        boolean creacionExito = false;
+        try{
+            Billetera billeteraNueva = new Billetera(2)
+            Cliente clienteNuevo =new Cliente("Carlos Veintemilla","0705786564",billeteraNueva);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
 }
--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -1,4 +1,47 @@
 package com.example.guayataxiv2;
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Conductor;
+import guayataxi.Taxi;
+
+import static org.junit.Assert.*;
 
 public class ConductorTest {
+    Conductor conductor = null;
+    Taxi taxi = null;
+
+    @Before
+    public void iniciarPruebas(){
+        conductor = new Conductor("Carlos Veintemilla", "0705786564");
+        taxi = new Taxi("asadasa", "marca", "aaa");
+    }
+
+    @Test
+    public void crearUnaConductor(){
+        boolean creacionExito = false;
+        try{
+            Conductor conductorNuevo = new Conductor("Carlos Veintemilla", "0705786564");
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void asignarTaxiLLeno(){
+        Taxi taxiNuevo = new Taxi("asadasa", "marca", "aaa");
+        boolean estado = conductor.asignarUnTaxi(taxiNuevo);
+
+        assertEquals(true, estado);
+    }
+
+    @Test
+    public void asignarTaxiVacio(){
+        Taxi taxiNuevo = null;
+        boolean estado = conductor.asignarUnTaxi(taxiNuevo);
+
+        assertEquals(false, estado);
+    }
 }
