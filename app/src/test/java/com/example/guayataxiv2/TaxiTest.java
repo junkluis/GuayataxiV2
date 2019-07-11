c5cab733103a3c271e6dd7924298a753ea1e3744
carangar <carlos.garzon@hotmail.es>
2019-07-11 17:55:27 -0500

    TaxiTest

--- app/src/test/java/com/example/guayataxiv2/TaxiTest.java
+++ app/src/test/java/com/example/guayataxiv2/TaxiTest.java
@@ -1,4 +1,40 @@
 package com.example.guayataxiv2;
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Taxi;
+
+
+import static org.junit.Assert.assertEquals;
 
 public class TaxiTest {
+    Taxi taxi = null;
+    @Before
+    public void iniciarTest(){
+        this.taxi = new Taxi("aaaa","bbbb",1);
+
+    }
+    @Test
+    public void crearTaxiNuevo(){
+        boolean creacionExito = false;
+        try{
+            Taxi nuevoCliente = new Taxi("aaaa","bbbb",1);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void asignarColorValido(){
+        String respuesta = this.taxi.setColor("Rojo");
+        assertEquals("Al taxi se le asigno el color: Rojo" ,respuesta);
+    }
+
+    @Test
+    public void asignarColorInvalido(){
+        String respuesta = this.taxi.setColor("Plateado");
+        assertEquals("Debe seleccionar un color dentor de la lista posibles" ,respuesta);
+    }
 }
