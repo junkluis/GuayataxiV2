2be6ea95766bd8780764b952bf3fdc772b0bed16
unknown <jlmasson@espol.edu.ec>
2019-07-12 03:58:06 -0500

    TaxiTest

--- app/src/test/java/com/example/guayataxiv2/TaxiTest.java
+++ app/src/test/java/com/example/guayataxiv2/TaxiTest.java
@@ -1,4 +1,31 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Taxi;
+
+import static org.junit.Assert.assertEquals;
+
 public class TaxiTest {
+
+    private Taxi taxiPrueba = null;
+
+    @Before
+    public void iniciarPruebas() {
+        taxiPrueba = new Taxi("GXY-2981", "NISSAN", 1);
+    }
+
+    @Test
+    public void setColorValido() {
+        String mensaje = taxiPrueba.setColor("Rojo");
+        assertEquals("Al taxi se le asigno el color: Rojo", mensaje);
+    }
+
+    @Test
+    public void setColorNoValido() {
+        String mensaje = taxiPrueba.setColor("Azul");
+        assertEquals("Debe seleccionar un color dentor de la lista posibles", mensaje);
+    }
+
 }
