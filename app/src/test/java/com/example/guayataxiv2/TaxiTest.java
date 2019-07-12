82e896f94a8e5e10494dee482691aaefdc147bde
rrobayo <rrobayo@espol.du.ec>
2019-07-12 08:32:08 -0500

    cambios

--- app/src/test/java/com/example/guayataxiv2/TaxiTest.java
+++ app/src/test/java/com/example/guayataxiv2/TaxiTest.java
@@ -9,14 +9,14 @@ import static org.junit.Assert.assertEquals;
 public class TaxiTest {
 
     @Test
-    public void colorImposible(){
+    public void colorIncorrect(){
         Taxi taxi = new Taxi("GQL502", "Toyota", 3);
         String colorImposible = taxi.setColor("Turquesa");
         assertEquals(colorImposible,"Debe seleccionar un color dentor de la lista posibles");
     }
 
     @Test
-    public void colorPosible(){
+    public void colorCorrect(){
         Taxi taxi = new Taxi("GQL502", "Toyota", 5);
         String colorPosible = taxi.setColor("Amarillo");
         assertEquals(colorPosible, "Al taxi se le asigno el color: Amarillo");
