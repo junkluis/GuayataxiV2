ff16257e0d3d88ccfa4d26a5c4a510a82bdf03d8
wmartine32 <wellinmart32@gmail.com>
2019-07-11 22:08:48 -0500

    Actualizacion de las clases erradas

--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -15,7 +15,8 @@ public class ConductorTest {
         conductorPrueba = new Conductor("Wellington Matinez", "4937276890");
         conductorSinTaxi = new Conductor("Clara Alcazar", "0955778979");
     }
-    
+
+    @Test
     public void asignarTaxi() {
         boolean asignacionExitosa = false;
         Taxi taxi = new Taxi("GXZ093", "Ferarari", 1);
@@ -27,26 +28,29 @@ public class ConductorTest {
         }
         assertEquals(true, asignacionExitosa);        
     }
-    
+
+    @Test
     public void sinTaxi() {
         Taxi taxi = null;
         boolean asignacionExitosa = false;        
         try {
             conductorPrueba.asignarUnTaxi(taxi);
-            asignacionExitosa = true;
-        } catch (AssertionError error){
             asignacionExitosa = false;
+        } catch (AssertionError error){
+            asignacionExitosa = true;
         }
-        assertEquals(true, asignacionExitosa);         
+        assertEquals(false, asignacionExitosa);
     }
-    
+
+    @Test
     public void verificacionCed(Conductor conductor) {
         boolean verificacion = false;
         verificacion = conductor.verificarCedula();
-        assertEquals(true, verificacion);         
+        assertEquals(true, verificacion);
 
     }
-    
+
+    @Test
     public void verificarCedulaDeConductores() {
         Conductor conductorSinPapeles = new Conductor("Jose Alcivar", "02");
         verificacionCed(conductorPrueba);
