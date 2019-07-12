ad2c9289ab49996022dbfaeea4ed1e05d61eb960
rrobayo <rrobayo@espol.du.ec>
2019-07-11 22:12:14 -0500

    ClienteTest

--- app/src/test/java/com/example/guayataxiv2/ClienteTest.java
+++ app/src/test/java/com/example/guayataxiv2/ClienteTest.java
@@ -1,4 +1,76 @@
 package com.example.guayataxiv2;
 
-public class ClienteTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Billetera;
+import guayataxi.Cliente;
+import guayataxi.Ubicacion;
+import guayataxi.Conductor;
+import guayataxi.Taxi;
+
+import static org.junit.Assert.*;
+
+public class ConductorTest {
+    Taxi taxiPrueba = null;
+    Conductor conductorPrueba = null;
+
+    @Before
+    public void iniciarPruebas(){
+        taxiPrueba = new Taxi("GSG-7978","KIA",1);
+
+        conductorPrueba = new Conductor("Leonardo Castro","0926385998");
+    }
+
+    @Test
+    public void crearUnNuevoConductor(){
+        boolean creacionExito = false;
+        try{
+            Conductor conductorNuevo = new Conductor("Leonardo Castro","0926385998");
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void asignarTaxiAlConductor(){
+        boolean valor = conductorPrueba.asignarUnTaxi(taxiPrueba);
+        assertEquals(true,valor);
+    }
+
+    @Test
+    public void asignarTaxiCreado(){
+        Taxi taxi = new Taxi("aaaaa","bbbbbb",1);
+        boolean estado = this.conductor.asignarUnTaxi(taxi);
+        assertEquals(true,estado);
+    }
+
+    @Test
+    public void asignarTaxiNoCreado(){
+        Taxi taxi = null;
+        boolean estado = this.conductorPrueba.asignarUnTaxi(taxi);
+        assertEquals(false,estado);
+    }
+    @Test
+    public void verificarCedulaValida(){
+        Conductor conductor= new Conductor("prueba Conductor", "0987654321");
+        boolean validez = conductor.verificarCedula();
+        assertEquals(true, validez);
+    }
+    @Test
+    public void verificarCedulaValid1(){
+        Conductor conductor= new Conductor("prueba Conductor", "9987654321");
+        boolean validez = conductor.verificarCedula();
+        assertEquals(false, validez);
+    }
+
+    @Test
+    public void verificarCedulaInalida2(){
+        Conductor conductor= new Conductor("prueba Conductor", "0654321");
+        boolean validez = conductor.verificarCedula();
+        assertEquals(false, validez);
+    }
+
 }
--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -1,4 +1,62 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Billetera;
+import guayataxi.Cliente;
+import guayataxi.Conductor;
+import guayataxi.Taxi;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConductorTest {
+
+
+    Billetera billeteraPrueba = null;
+
+    Cliente clientePrueba = null;
+    Taxi taxi = null;
+
+
+    @Before
+    public void iniciarPruebas(){
+        billeteraPrueba = new Billetera(2);
+        billeteraPrueba.agregarFondos(10);
+        clientePrueba = new Cliente("Pedro","09200558696",billeteraPrueba);
+
+    }
+
+    @Test
+    public void crearUnaNuevoConductor(){
+        boolean creacionExito = false;
+        try{
+            Conductor clienteLuis = new Conductor("Luis","09200558696");
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+    @Test
+    public void bloquearBilletera(){
+        boolean estado = billeteraPrueba.habilitarBilletera(false);
+        assertEquals(false, estado);
+    }
+
+    @Test
+    public void registrarCasa(){
+        boolean creacionExito = false;
+        try{
+            Ubicacion casaLuis = new Ubicacion(22,25,12);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+
+
 }
