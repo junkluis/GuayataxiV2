f14670b67be2f1185ea507d6251591a13ff97bcb
dfsaigua <dfsaigua@espol.edu.ec>
2019-07-12 04:12:58 -0500

    correcion6 Ubicaciontest

--- app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
+++ app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
@@ -27,13 +27,20 @@ public class UbicacionTest {
     }
 
     @Test
-    public void preciocorrecto(){
+    public void preciocorrectoprimeravalidacion(){
 
-        Ubicacion miCasa = new Ubicacion(12.2f,12.2f,15.0f);
-        float precio = miCasa.calcularPrecio();
-        assertEquals(precio,5.0f,0.5f);
+        Ubicacion ubicacion = new Ubicacion(10.6f,10.3f,18.0f);
+        float valor = ubicacion.calcularPrecio();
+        assertEquals(valor,5.0f,0.5f);
 
+    }
 
+    @Test
+    public void preciocorrectosegundavalidacion(){
+
+        Ubicacion ubicacion1 = new Ubicacion(9.8f,16.9f,40.0f);
+        float valor1 = ubicacion1.calcularPrecio();
+        assertEquals(valor1,10.0f,0.5f);
 
     }
 
