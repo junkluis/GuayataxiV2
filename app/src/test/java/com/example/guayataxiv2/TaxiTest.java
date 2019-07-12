dd73d2d632fb848036b41748a71501e83edeb078
dfsaigua <dfsaigua@espol.edu.ec>
2019-07-12 04:34:42 -0500

    taxitest

--- app/src/test/java/com/example/guayataxiv2/TaxiTest.java
+++ app/src/test/java/com/example/guayataxiv2/TaxiTest.java
@@ -1,4 +1,40 @@
 package com.example.guayataxiv2;
+package com.example.guayataxiv2;
+import guayataxi.Cliente;
+import guayataxi.Ubicacion;
+import guayataxi.Conductor;
+import guayataxi.Taxi;
+import org.junit.Before;
+import org.junit.Test;
+import guayataxi.Billetera;
+import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertTrue;
+
 
 public class TaxiTest {
+
+
+
+    Taxi taxi1 = null;
+
+
+    @Before
+    public void Pruebastaxi(){
+        taxi1 = new Taxi("GBN433","MAZDA",11);
+
+
+
+    }
+
+    @Test
+    public void creartaxi(){
+        boolean creacionExito = false;
+        try{
+            Taxi taxi = new Taxi("GOV623","CHEVROLET",14);
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
 }
