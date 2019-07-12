83857d88c3f65fec0698c7cecf05271307ec7042
unknown <jlmasson@espol.edu.ec>
2019-07-12 03:52:07 -0500

    UbicacionTest

--- app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
+++ app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
@@ -1,4 +1,57 @@
 package com.example.guayataxiv2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Ubicacion;
+
+import static org.junit.Assert.*;
+
 public class UbicacionTest {
+
+    private Ubicacion u1 = null;
+    private Ubicacion u2 = null;
+    private Ubicacion u3 = null;
+    private Ubicacion u4 = null;
+    private Ubicacion u5 = null;
+
+    @Before
+    public void iniciarPruebas() {
+        u1 = new Ubicacion(20F, 30F, -1);
+        u2 = new Ubicacion(20F, 30F, 5);
+        u3 = new Ubicacion(20F, 30F, 29.9F);
+        u4 = new Ubicacion(50.01F, 30F, 30.01F);
+        u5 = new Ubicacion(30.01F, 50.01F, 30.01F);
+    }
+
+    @Test
+    public void calcularPrecioDistanciaNegativa() {
+        float precio = u1.calcularPrecio();
+        assertTrue(precio == 0);
+    }
+
+    @Test
+    public void calcularPrecioDistanciaC1() {
+        float precio = u2.calcularPrecio();
+        assertTrue(precio == 2);
+    }
+
+    @Test
+    public void calcularPrecioDistanciaC2() {
+        float precio = u3.calcularPrecio();
+        assertTrue(precio == 5);
+    }
+
+    @Test
+    public void calcularPrecioDistanciaC3() {
+        float precio = u4.calcularPrecio();
+        assertTrue(precio == 10);
+    }
+
+    @Test
+    public void calcularPrecioDistanciaC4() {
+        float precio = u5.calcularPrecio();
+        assertTrue(precio == 15);
+    }
+
 }
