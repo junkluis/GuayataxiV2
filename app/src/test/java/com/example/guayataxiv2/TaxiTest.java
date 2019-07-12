7818d568e15a4c7039e5c86babf057314b731f5a
Xavier Andrade <xav_andrade@hotmail.com>
2019-07-12 10:45:24 -0500

    Tests de Taxi

--- app/src/test/java/com/example/guayataxiv2/TaxiTest.java
+++ app/src/test/java/com/example/guayataxiv2/TaxiTest.java
@@ -1,11 +1,26 @@
 package com.example.guayataxiv2;
 
 import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import guayataxi.Taxi;
 
 public class TaxiTest {
 
+    Taxi taxiSinColor = null;
+
     @Before
     public void initTest() {
+        this.taxiSinColor = new Taxi(
+                "GYE1900",
+                "Volvo",
+                101
+        );
+    }
 
+    @Test
+    public void testSettearColorInvalido() {
+        String mensaje = this.taxiSinColor.setColor("Violeta");
+        assertEquals("Debe seleccionar un color dentor de la lista posibles", mensaje);
     }
 }
