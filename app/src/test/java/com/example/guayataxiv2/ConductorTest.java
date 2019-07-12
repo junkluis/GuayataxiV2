8a7cd926d70ba9ccbc455384c3eb5ae95065b226
dfsaigua <dfsaigua@espol.edu.ec>
2019-07-12 01:59:58 -0500

    Conductortest

--- app/src/test/java/com/example/guayataxiv2/ConductorTest.java
+++ app/src/test/java/com/example/guayataxiv2/ConductorTest.java
@@ -1,4 +1,48 @@
 package com.example.guayataxiv2;
+import guayataxi.Cliente;
+import guayataxi.Ubicacion;
+import guayataxi.Taxi;
+import org.junit.Before;
+import org.junit.Test;
+import guayataxi.Billetera;
+import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertTrue;
 
 public class ConductorTest {
+
+    Taxi taxiprueba = null;
+    Conductor conductor=null;
+    Ubicacion ubicacion1=null;
+
+
+    @Before
+    public void Pruebasconductor(){
+        taxiprueba = new Taxi("GOV456","CHEVROLET",12);
+        conductor =new Conductor("Daniel Fernando Saigua Labre","0952475688");
+
+
+    }
+
+    @Test
+    public void crearconductor(){
+        boolean creacionExito = false;
+        try{
+            Conductor conductor = new Conductor("Daniela Mera","0935234519");
+            creacionExito = true;
+        } catch (AssertionError error){
+            creacionExito = false;
+        }
+        assertEquals(true, creacionExito);
+    }
+
+   
+
+    @Test
+    public void comprobarcedula(){
+        conductor =new Conductor("Daniel Fernando Saigua Labre","0952475688");
+        boolean valorcorrecto = conductor.verificarCedula();
+        assertTrue(valorcorrecto);
+    }
+
+
 }
