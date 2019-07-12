947306d76ff72432a3bdf6b8b907e178dfd0726a
Xavier Andrade <xav_andrade@hotmail.com>
2019-07-12 10:51:03 -0500

    Tests for Ubicacion

--- app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
+++ app/src/test/java/com/example/guayataxiv2/UbicacionTest.java
@@ -1,11 +1,23 @@
 package com.example.guayataxiv2;
 
+import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Test;
+
+import guayataxi.Ubicacion;
 
 public class UbicacionTest {
+    Ubicacion ubicacion = null;
 
     @Before
     public void initTest() {
+        this.ubicacion = new Ubicacion(1.34534f, 0.7234234f, 15f);
 
+    }
+
+    @Test
+    public void testCalcularPrecio() {
+        float costo = this.ubicacion.calcularPrecio();
+        assertEquals(5f, costo, 0);
     }
 }
